2c62f2147ee58160f3fdcd1cf88972b2f9ebc6ef
hadarAdiE <116706181+hadarAdiE@users.noreply.github.com>
2023-02-20 10:58:37 +0200

    finished project, only for pc

--- javas.js
+++ javas.js
@@ -1,6 +1,10 @@
+let body = document.querySelector('body');
 let container = document.querySelector('.container');
 let divs = [];
 let btn = document.querySelector('.btn');
+let rgbColors = [];
+
+
 // let 
 
 for (let index = 0; index < 256; index++) {
@@ -10,18 +14,12 @@ for (let index = 0; index < 256; index++) {
 }
 
 
+
+
 btn.addEventListener('click', grid);
-
-function color(e) {
-    e.target.style.background = 'blue';
-}
-
-function clear() {
-    divs.forEach(div => {
-        container.removeChild(div);
-    });
-    divs = [];
-}
+divs.forEach(div => {
+    div.addEventListener('mouseover', color, {once:true})
+});
 
 function grid() {
     let num = prompt('how many squares do you want in this grid (number has to be between 0-100)?');
@@ -37,6 +35,7 @@ function grid() {
         let height = 600 / num;
         for (let index = 0; index < total; index++) {
             divs.push(document.createElement('div'));
+            // body.appendChild(divs[index]);
             container.appendChild(divs[index]);
             divs[index].style = `width: ${widthDiv}px; height: ${height}px; padding: 0px`;
         }
@@ -44,15 +43,66 @@ function grid() {
     }
 
     divs.forEach(div => {
-        div.addEventListener('mouseover', color)
+        div.addEventListener('mouseover', color, {once: true})
     });
 
 }
 
+function clear() {
+    divs.forEach(div => {
+        container.removeChild(div);
+    });
+    divs = [];
+}
+function randomNum() {
+    let num1 = Math.floor(Math.random() * 256);
+    // console.log(`num1 is `+num1);
+    return num1;
+}
 
-divs.forEach(div => {
-    div.addEventListener('mouseover', color)
-});
+
+function color(e) {
+    dark = 100;
+    for (i = 0; i < 3; i++) {
+        rgbColors[i] = randomNum();
+    }
+    e.target.style.background = `rgb(${rgbColors[0]},${rgbColors[1]},${rgbColors[2]})`;
+    //initializing brightness
+    e.target.style.filter = `brightness(100%)`;
+    
+    e.target.addEventListener('mouseover', goBlackColor);
+    
+}
+
+
+function goBlackColor(e) {
+   
+    let initialB = e.target.style.filter;
+    let filterNum = Number(initialB.replace(/\D|\s/g, ""));
+    e.target.style.filter = `brightness(${filterNum - 10}%)`;
+   
+   
+    
+    
+    console.log(e.target.style.filter);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // container.appendChild(divs);    
 
